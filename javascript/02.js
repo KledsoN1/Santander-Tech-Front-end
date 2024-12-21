184b241f47005a7d93d65ed6a83675d3ea90e701
KledsoN1 <kledsongame@gmail.com>
2024-12-20 21:08:20 -0300

    at

---
+++ javascript/02.js
@@ -0,0 +1,10 @@
+const nota1 = 10
+const nota2 = 8 
+const somaDasNotas = nota1 + nota2 
+const mediaDasNotas = somaDasNotas /2 
+
+console.log('A soma das notas é =', somaDasNotas)
+console.log('A media das notas é =', mediaDasNotas)
+
+const numero = 16
+console.log('A raíz quadrada de 16 é =', Math.sqrt(numero))
---
+++ javascript/aula06.js
@@ -0,0 +1,10 @@
+console.log('Hello World!')
+var nome = 'Kledson'
+var idade = 18
+var altura = 1.71
+var estudando = true
+
+console.log(nome)
+console.log(idade)
+console.log(altura)
+console.log(estudando)
